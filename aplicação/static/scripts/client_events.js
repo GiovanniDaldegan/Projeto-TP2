bc3daf84a23f557ab03dc35390aa43eb1eda69d1
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-23 21:38:56 -0300

    feat: requisição da lista de produtos

---
+++ aplicação/static/scripts/client_events.js
@@ -0,0 +1,6 @@
+export function request_product_list(socketio, product_name, filters) {
+    socketio.emit("get-product-list", {
+        product_name : product_name,
+        filters      : filters
+    });
+}
