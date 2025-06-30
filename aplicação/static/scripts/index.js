bd0260d23befd34578302594dafc63273dfe17a8
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-06-30 17:53:33 -0300

    feat: estrutura dos próximos módulos do cliente
    padronização dos módulos do cliente

--- aplicação/static/scripts/index.js
+++ aplicação/static/scripts/index.js
@@ -1,11 +1,29 @@
-import { searchSetupHTML, searchListenersSocketio} from "./product_search.js";
+import { productSearchSetupHTML, productSearchSetupListeners} from "./product_search.js";
+import { shoppingListSetupHTML, shoppingListSetupListeners } from "./shopping_list.js";
+import { registerSetupHTML, registerSetupListeners } from "./register.js";
+import { loginSetupHTML, loginSetupListeners} from "./login.js";
 
 export var socketio = io();
 
 
 function setupProductSearch() {
-  searchListenersSocketio(socketio);
-  searchSetupHTML(socketio);
+  productSearchSetupListeners();
+  productSearchSetupHTML();
+}
+
+function setupShoppingList() {
+  shoppingListSetupHTML();
+  shoppingListSetupListeners();
+}
+
+function setupRegister() {
+  registerSetupHTML();
+  registerSetupListeners();
+}
+
+function setupLogin() {
+  loginSetupHTML();
+  loginSetupListeners();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
---
+++ aplicação/static/scripts/login.js
@@ -0,0 +1,14 @@
+import { socketio } from "./index.js";
+
+// Funções emissoras de requisição
+
+
+// Funções próprias do módulo
+
+
+// setup dos listeners de eventos da tela
+export function loginSetupListeners() {}
+
+
+// setup dos eventos do HTML da tela
+export function loginSetupHTML() {}
