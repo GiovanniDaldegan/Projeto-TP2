c1c236871cc63785608e8a43ac2890cc46052a72
TauaFrade <tau.valentim@gmail.com>
2025-07-12 10:53:04 -0300

    feat: Cria função que retorna melhores opções para comprar uma lista dada. OBS: ainda não foi bem testada

--- aplicação/modules/i_shopping_list.py
+++ aplicação/modules/i_shopping_list.py
@@ -17,3 +17,45 @@ def send_product_list(list_id):
     shopping_list = db_controller.get_product_list(list_id)
 
     socketio.emit("shopping-list", shopping_list)
+
+@socketio.on("get-best-market")
+def get_best_market(list:list[dict]):
+    n_items = len(list)
+    complete = []
+    relation = {}
+    best = [0, 0]
+    for item in list:
+        sellers = db_controller.get_product_sellers(item['product_id'])
+        for s in sellers:
+            market = s['id_market']
+            if(market in relation):
+                relation[market]['available'] += 1
+                relation[market]['total_price'] += (s['price']  * item['quantity'])
+            else:
+                relation[market] = {
+                    "id_market" : market,
+                    "market_name" : s["market_name"],
+                    "available" : 1,
+                    "total_price" : s['price']  * item['quantity']
+                }
+            if(relation[market]['available'] > best[0]):
+                best[0] = relation[market]['available']
+                best[1] = market
+            elif((relation[market]['available'] == best[0]) and (relation[market]['total_price'] < relation[best[1]]['total_price'])):
+                best[0] = relation[market]['available']
+                best[1] = market
+            if(relation[market]['available'] == n_items):
+                complete.append(relation[market])
+    if(complete):
+        #sort complete array by total_price
+        out = {
+            "complete" : True,
+            "options" : complete
+        }
+        return out
+    else:
+        out = {
+            "complete" : False,
+            "options" : relation[best[1]]
+        }
+        return out
