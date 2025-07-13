39754e587530ff9abfb59c978a561b09b1643a42
ricardo de carvalho nabuco 2321021360 <ricardocnabuco@gmail.com>
2025-07-13 02:08:16 -0300

    Adicionando uma rota para o cadastro de produto

--- aplicação/app.py
+++ aplicação/app.py
@@ -2,6 +2,8 @@
 
 import os
 
+from flask import request, redirect, url_for
+from werkzeug.utils import secure_filename
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
@@ -44,6 +46,32 @@ def create_app():
 
     # índice da aplicação
     @app.route("/")
+    
+    @app.route("/produto/novo", methods=["POST"])
+    def cadastrar_produto():
+        nome = request.form["nome"]
+        # Se o formulário tiver campos para mercado e preço, inclua-os:
+        # id_market = int(request.form["id_market"])
+        # preco = float(request.form["preco"])
+        imagem = request.files["imagem"]
+
+        if imagem:
+            nome_arquivo = secure_filename(imagem.filename)
+            caminho_pasta = os.path.join("aplicação", "static", "img", "produtos")
+            os.makedirs(caminho_pasta, exist_ok=True)
+            caminho_arquivo = os.path.join(caminho_pasta, nome_arquivo)
+            imagem.save(caminho_arquivo)
+            caminho_relativo = f"img/produtos/{nome_arquivo}"
+        else:
+            caminho_relativo = None
+
+        # Ajuste a chamada conforme o método implementado no seu DBController
+        db_controller.create_product(nome, caminho_relativo)
+        # Se houver campos de mercado/preço:
+        # db_controller.create_product(nome, id_market, preco, caminho_relativo)
+
+        return redirect(url_for("index"))
+
     def index():
         return render_template("index.html")
 
